89d6ec82ccfc923e10e2b86f9ca484b2d12a2e2e
hara Prasad <haraprasad45@gmail.com>
2019-12-24 12:01:58 +0530

    target blank added to links, change box shadow in social icons

--- src/components/About.js
+++ src/components/About.js
@@ -8,21 +8,21 @@ export default () => {
           <span className="text-primary"> Prasad</span>
         </h1>
         <div className="subheading mb-5">hitec city · hyderabad, india · +91 801-884-9774 ·
-          <a href="mailto:haraprasad45@gmail.com">haraprasad45@gmail.com</a>
+          <a href="mailto:haraprasad45@gmail.com" target="blank">haraprasad45@gmail.com</a>
         </div>
         <p className="lead"><b><span className="text-primary">Hi, </span></b>I am a frontend developer. I develop web sites and web applications using modern front-end web technologies. The tech I am currently loving is <span className="text-primary">ReactJs.</span></p>
-        <p className="lead mb-5">Need a developer? Give me a shout at <u><a href="mailto:haraprasad45@gmail.com">haraprasad45@gmil.com</a></u> or feel free to check out my <a className='text-primary'><u>resume.</u></a></p>
+        <p className="lead mb-5">Need a developer? Give me a shout at <u><a href="mailto:haraprasad45@gmail.com" target="blank">haraprasad45@gmil.com</a></u> or feel free to check out my <a className='text-primary'><u>resume.</u></a></p>
         <div className="social-icons">
-          <a href="#">
+          <a href="#" target="blank">
             <i className="fab fa-linkedin-in"></i>
           </a>
-          <a href="#">
+          <a href="#" target="blank">
             <i className="fab fa-github"></i>
           </a>
-          <a href="#">
+          <a href="#" target="blank">
             <i className="fab fa-instagram"></i>
           </a>
-          <a href="#">
+          <a href="#" target="blank">
             <i className="fab fa-free-code-camp"></i>
           </a>
         </div>
