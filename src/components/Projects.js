67462e5f105cb1164c9b83210a7babe8c92fea9f
hara Prasad <haraprasad45@gmail.com>
2019-12-24 12:24:14 +0530

    change all "class" to reactjs "className"

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,30 +3,30 @@ import React from "react";
 export default () => {
     return (
 
-        <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="projects">
-            <div class="w-100">
-                <h2 class="mb-5">projects</h2>
+        <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="projects">
+            <div className="w-100">
+                <h2 className="mb-5">projects</h2>
 
-                <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-                    <div class="resume-content">
-                        <h3 class="mb-0">University of Colorado Boulder</h3>
-                        <div class="subheading mb-3">Bachelor of Science</div>
+                <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div className="resume-content">
+                        <h3 className="mb-0">University of Colorado Boulder</h3>
+                        <div className="subheading mb-3">Bachelor of Science</div>
                         <div>Computer Science - Web Development Track</div>
                         <p>GPA: 3.23</p>
                     </div>
-                    <div class="resume-date text-md-right">
-                        <span class="text-primary">August 2006 - May 2010</span>
+                    <div className="resume-date text-md-right">
+                        <span className="text-primary">August 2006 - May 2010</span>
                     </div>
                 </div>
 
-                <div class="resume-item d-flex flex-column flex-md-row justify-content-between">
-                    <div class="resume-content">
-                        <h3 class="mb-0">James Buchanan High School</h3>
-                        <div class="subheading mb-3">Technology Magnet Program</div>
+                <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
+                    <div className="resume-content">
+                        <h3 className="mb-0">James Buchanan High School</h3>
+                        <div className="subheading mb-3">Technology Magnet Program</div>
                         <p>GPA: 3.56</p>
                     </div>
-                    <div class="resume-date text-md-right">
-                        <span class="text-primary">August 2002 - May 2006</span>
+                    <div className="resume-date text-md-right">
+                        <span className="text-primary">August 2002 - May 2006</span>
                     </div>
                 </div>
 
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,61 +2,61 @@ import React from 'react';
 
 export default () => {
     return (
-        <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
-      <div class="w-100">
-        <h2 class="mb-5">Skills</h2>
+        <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
+      <div className="w-100">
+        <h2 className="mb-5">Skills</h2>
 
-        <div class="subheading mb-3">Programming Languages &amp; Tools</div>
-        <ul class="list-inline dev-icons">
-          <li class="list-inline-item">
-            <i class="fab fa-html5"></i>
+        <div className="subheading mb-3">Programming Languages &amp; Tools</div>
+        <ul className="list-inline dev-icons">
+          <li className="list-inline-item">
+            <i className="fab fa-html5"></i>
           </li>
-          <li class="list-inline-item">
-            <i class="fab fa-css3-alt"></i>
+          <li className="list-inline-item">
+            <i className="fab fa-css3-alt"></i>
           </li>
-          <li class="list-inline-item">
-            <i class="fab fa-js-square"></i>
-          </li>
-          
-          <li class="list-inline-item">
-            <i class="fab fa-react"></i>
+          <li className="list-inline-item">
+            <i className="fab fa-js-square"></i>
           </li>
           
-          <li class="list-inline-item">
-            <i class="fab fa-sass"></i>
+          <li className="list-inline-item">
+            <i className="fab fa-react"></i>
+          </li>
+          
+          <li className="list-inline-item">
+            <i className="fab fa-sass"></i>
           </li>
           
           
-          <li class="list-inline-item">
-            <i class="fab fa-git-alt"></i>
-          </li>
-
-          <li class="list-inline-item">
-            <i class="fab fa-bootstrap"></i>
+          <li className="list-inline-item">
+            <i className="fab fa-git-alt"></i>
           </li>
 
-          <li class="list-inline-item">
-            <i class="fab fa-linux"></i>
+          <li className="list-inline-item">
+            <i className="fab fa-bootstrap"></i>
+          </li>
+
+          <li className="list-inline-item">
+            <i className="fab fa-linux"></i>
           </li>
          
-          <li class="list-inline-item">
-            <i class="fab fa-npm"></i>
+          <li className="list-inline-item">
+            <i className="fab fa-npm"></i>
           </li>
         </ul>
 
-        <div class="subheading mb-3">Workflow</div>
-        <ul class="fa-ul mb-0">
+        <div className="subheading mb-3">Workflow</div>
+        <ul className="fa-ul mb-0">
           <li>
-            <i class="fa-li fa fa-check"></i>
+            <i className="fa-li fa fa-check"></i>
             Mobile-First, Responsive Design</li>
           <li>
-            <i class="fa-li fa fa-check"></i>
+            <i className="fa-li fa fa-check"></i>
             Functional Programming &amp; OOPs</li>
           <li>
-            <i class="fa-li fa fa-check"></i>
+            <i className="fa-li fa fa-check"></i>
             Web Design concepts</li>
           <li>
-            <i class="fa-li fa fa-check"></i>
+            <i className="fa-li fa fa-check"></i>
             AJEX &amp; JSON</li>
         </ul>
       </div>
