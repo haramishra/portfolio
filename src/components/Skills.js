59f80c279f8150c4a1f00f5e9b817387bc66b46f
hara Prasad <haraprasad45@gmail.com>
2019-12-25 12:53:46 +0530

    remove redundent jsx code

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,65 +1,61 @@
 import React from 'react';
 
-export default () => {
+let programmingToolClass = [
+  "fab fa-html5",
+  "fab fa-css3-alt",
+  "fab fa-js-square",
+  "fab fa-react",
+  "fab fa-sass",
+  "fab fa-git-alt",
+  "fab fa-bootstrap",
+  "fab fa-linux",
+  "fab fa-npm"
+];
+
+let workflow = [
+  "Mobile-First, Responsive Design",
+  "Functional Programming &amp; OOPs",
+  "Web Design concepts",
+  "AJEX &amp; JSON"
+]
+
+let displayProgTools = (proTool) => {
+  return programmingToolClass && programmingToolClass.map((item, index) => {
     return (
-        <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
+      <li className="list-inline-item" key={index}>
+        <i className={item}></i>
+      </li>
+    );
+  })
+}
+
+export default () => {
+  return (
+    <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
       <div className="w-100">
         <h2 className="mb-5">Skills</h2>
 
         <div className="subheading mb-3">Programming Languages &amp; Tools</div>
         <ul className="list-inline dev-icons">
-          <li className="list-inline-item">
+          {/* <li className="list-inline-item">
             <i className="fab fa-html5"></i>
-          </li>
-          <li className="list-inline-item">
-            <i className="fab fa-css3-alt"></i>
-          </li>
-          <li className="list-inline-item">
-            <i className="fab fa-js-square"></i>
-          </li>
-          
-          <li className="list-inline-item">
-            <i className="fab fa-react"></i>
-          </li>
-          
-          <li className="list-inline-item">
-            <i className="fab fa-sass"></i>
-          </li>
-          
-          
-          <li className="list-inline-item">
-            <i className="fab fa-git-alt"></i>
-          </li>
+          </li> */}
+          {
+            displayProgTools(programmingToolClass)
+          }
 
-          <li className="list-inline-item">
-            <i className="fab fa-bootstrap"></i>
-          </li>
-
-          <li className="list-inline-item">
-            <i className="fab fa-linux"></i>
-          </li>
-         
-          <li className="list-inline-item">
-            <i className="fab fa-npm"></i>
-          </li>
         </ul>
 
         <div className="subheading mb-3">Workflow</div>
         <ul className="fa-ul mb-0">
-          <li>
-            <i className="fa-li fa fa-check"></i>
-            Mobile-First, Responsive Design</li>
-          <li>
-            <i className="fa-li fa fa-check"></i>
-            Functional Programming &amp; OOPs</li>
-          <li>
-            <i className="fa-li fa fa-check"></i>
-            Web Design concepts</li>
-          <li>
-            <i className="fa-li fa fa-check"></i>
-            AJEX &amp; JSON</li>
+
+          {
+            workflow && workflow.map((item, index) => <li key={index}>
+              <i className="fa-li fa fa-check"></i>
+              {item}</li>)
+          }
         </ul>
       </div>
     </section>
-    );
+  );
 }
