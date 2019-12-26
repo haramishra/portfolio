732b9fd2f192971b3f8d76d853d1d1df48b80b2d
hara Prasad <haraprasad45@gmail.com>
2019-12-26 12:28:56 +0530

    jf

--- src/components/Intrests.js
+++ src/components/Intrests.js
@@ -6,7 +6,7 @@ export default () => {
             <div className="w-100">
                 <h2 className="mb-5">Interests</h2>
                 <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
-                <p className="mb-0">When forced indoors, I follow a number of sci-fi and drama genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+                <p className="mb-0">When forced indoors, I follow a number of sci-fi, thriller and drama genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
             </div>
         </section>
     );
