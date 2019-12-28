7ae9a8e6c5085d2add9d70a8a3b89cc6609558c8
hara prasad mishra <haraprasad45@gmail.com>
2019-12-28 19:29:54 +0530

    Update About.js
    removed redundant jsx

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
+const socialIcons = [
+  {
+    icon: "fa-linkedin-in",
+    link: "https://www.linkedin.com/in/hara-prasad-mishra-946a14142/"
+  }, {
+    icon: "fa-github",
+    link: "https://github.com/haramishra"
+  }, {
+    icon: "fa-medium-m",
+    link: "https://medium.com/@haraprasad45"
+  }, {
+    icon: "fa-codepen",
+    link: "https://codepen.io/collection/Xdeodw"
+  }
+];
+
+const displaySocialIcon = (icon) => icon.map((item, index) => {
+  return (
+    <a href={item.link} key={index} target="blank">
+      <i className={`fab ${item.icon}`}></i>
+    </a>
+  );
+})
+
 export default () => {
   return (
     <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
@@ -13,7 +37,7 @@ export default () => {
         <p className="lead"><b><span className="text-primary">Hi, </span></b>I am a frontend developer. I develop web sites and web applications using modern front-end web technologies. The tech I am currently loving is <span className="text-primary">ReactJs.</span></p>
         <p className="lead mb-5">Need a developer? Give me a shout at <u><a href="mailto:haraprasad45@gmail.com" target="blank">haraprasad45@gmil.com</a></u> or feel free to check out my <a className='text-primary'><u>resume.</u></a></p>
         <div className="social-icons">
-          <a href="#" target="blank">
+          {/* <a href="#" target="blank">
             <i className="fab fa-linkedin-in"></i>
           </a>
           <a href="#" target="blank">
@@ -24,7 +48,8 @@ export default () => {
           </a>
           <a href="#" target="blank">
             <i className="fab fa-free-code-camp"></i>
-          </a>
+          </a> */}
+          {socialIcons && displaySocialIcon(socialIcons)}
         </div>
       </div>
     </section>
