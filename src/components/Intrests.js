e05a4d072b52e353754b095aae21486a0400eb2a
hara prasad mishra <haraprasad45@gmail.com>
2020-01-03 14:58:04 +0530

    Intrests added

--- src/components/Intrests.js
+++ src/components/Intrests.js
@@ -5,8 +5,8 @@ export default () => {
         <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
             <div className="w-100">
                 <h2 className="mb-5">Interests</h2>
-                <p>Apart from being a web developer. In the winter, I sleep till 2pm. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
-                <p className="mb-0">When forced indoors, I follow a number of sci-fi, thriller and drama genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+                <p>Apart from web development, I like to spend most of my time exploring new technological advancement in frontend web technologies.</p>
+                <p className="mb-0">I love travelling! I travelled over ten cities across the country. I also like to indulge myself in culinary activities and I follow several si-fi and drama genre movies and tv-shows in my leisure time.</p>
             </div>
         </section>
     );
