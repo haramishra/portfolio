a856f576f82f92c16eee21386579991ca469118e
hara prasad mishra <haraprasad45@gmail.com>
2020-01-07 12:59:18 +0530

    minor changes

--- src/components/Intrests.js
+++ src/components/Intrests.js
@@ -6,7 +6,7 @@ export default () => {
             <div className="w-100">
                 <h2 className="mb-5">Interests</h2>
                 <p>Apart from web development, I like to spend most of my time exploring new technological advancement in frontend web technologies.</p>
-                <p className="mb-0">I love travelling! I travelled over ten cities across the country. I also like to indulge myself in culinary activities and I follow several si-fi and drama genre movies and tv-shows in my leisure time.</p>
+                <p className="mb-0">I love travelling! I travelled over ten cities across the country. I also like to indulge myself in culinary activities and I follow several si-fi and thriller genre movies and tv-shows in my leisure time.</p>
             </div>
         </section>
     );
